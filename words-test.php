023c940640ded7cdd305b785cba2e6fd2479d532
Jess Hunte <jess.hunte@gmail.com>
2016-11-15 02:11:29 +0000

    Adding text page test

---
+++ words-test.php
@@ -0,0 +1,100 @@
+<html>
+  <head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="viewport" content="width=device-width">
+<title>Inhuman [a webcomic] : updates WEEKLY</title>
+<meta name="keywords" content="webcomics comics comic webcomic inhuman hekshanian nikitak soshika soshi lypha kyotoshi solla koji scifi sci fi sci-fi manga hekshano icarus">
+<meta name="description" content="a sci-fi webcomic">
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+
+</head>
+<body>
+
+<div class="body">
+	<header>
+		<?php 
+			//Ads and whatnot go here
+		?>
+	</header>
+	<div class="content-wrapper">
+		<nav class="clearfix">
+			<a href="/" class="index-link"></a>
+			<section class="tabs">
+				<a href="#archives" class="link">archives</a>
+				<a href="#about" class="link">about</a>
+				<a href="#shop" class="link">shop</a>
+				<a href="#extra" class="link">extra</a>
+			</section>
+		</nav>
+		<section class="main">
+			<aside class="left">
+				<div class="last-update">
+					<p>6/6/66</p>
+				</div>
+				<div class="social-media">
+					<a href="#tumblr" class="tumblr"></a>
+					<a href="#twitter" class="twitter"></a>
+					<a href="#facebook" class="facebook"></a>
+				</div>
+			</aside>
+			<aside class="right">
+				<a href="#patreon" class="patreon"></a>
+			</aside>
+			<section class="center">
+				<header class="text-title">
+					<h2>Title</h1>
+				</header>
+				<article class="text-content">
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie vehicula faucibus. Quisque eu leo id elit sodales rhoncus sed id massa. Etiam ac sagittis nisi, a sollicitudin magna. Nullam tincidunt elementum enim, non imperdiet nisi pulvinar eu. Vivamus pellentesque turpis nec velit blandit feugiat. Donec non ex id elit ultricies feugiat vel eget quam. Etiam felis quam, varius non ex sed, ullamcorper venenatis erat. Mauris a felis eu velit hendrerit lacinia. Duis semper nulla et metus mattis convallis. Aliquam at massa bibendum, molestie diam sit amet, ultrices tellus. Sed vel convallis tortor, non bibendum mi. Maecenas vitae sapien mauris. Suspendisse potenti.
+					</p>
+					<p>
+					Fusce eu ultricies purus, non mollis nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam auctor sapien non consectetur vestibulum. Suspendisse eget dolor et nibh aliquam tristique vel posuere nunc. Duis vestibulum ultrices luctus. Proin tincidunt felis justo. Sed rhoncus egestas elementum. Nam rutrum arcu scelerisque ante tincidunt, et malesuada nulla bibendum. Praesent vehicula gravida imperdiet. Mauris vitae sem eu dolor laoreet consequat. Integer dignissim elit vulputate, malesuada erat et, tincidunt massa. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc commodo lectus ac purus sollicitudin venenatis. Aliquam vel sollicitudin arcu. Suspendisse congue, felis nec congue tempus, ex est condimentum turpis, laoreet mollis nulla enim sit amet justo. Morbi imperdiet dui ut convallis egestas.
+					</p>
+					<p>
+					Vestibulum vestibulum, metus eu ullamcorper finibus, ante ex pellentesque magna, in bibendum orci ligula nec tellus. Mauris vulputate ultrices nisi id tincidunt. Vestibulum non lacus ac ante tincidunt malesuada eleifend sit amet arcu. Suspendisse efficitur commodo commodo. Sed faucibus semper odio. Donec ac dictum augue, non efficitur quam. Nam vel enim iaculis, egestas augue non, sodales metus. Vestibulum nec lectus lectus. Phasellus lorem lectus, rhoncus in tincidunt ut, tempor sed orci. Ut odio nibh, feugiat vel feugiat id, porta fermentum mi. Suspendisse ultrices mi semper, blandit mi a, varius dolor. Donec consequat ut tellus in aliquet. In hac habitasse platea dictumst. Nulla in egestas metus.
+					</p>
+					<p>
+					Aenean quis metus eu erat mattis aliquam non at sapien. Mauris orci felis, ultrices at convallis quis, facilisis sit amet nisi. Sed volutpat, dui eget commodo ornare, libero ex facilisis magna, sit amet molestie diam leo eu erat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam finibus egestas nibh vitae semper. Phasellus accumsan tortor lorem, nec tempus leo tristique vitae. Fusce non ullamcorper dolor. Proin vehicula urna a tincidunt viverra. In at congue tellus. Vestibulum nec leo scelerisque, feugiat orci sit amet, tristique libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut pellentesque diam tellus. Quisque eget metus id leo varius commodo. Cras nec elit eu magna posuere rhoncus. Curabitur sagittis vitae lorem vel convallis.
+					</p>
+					<p>
+					Pellentesque venenatis, orci et sodales imperdiet, velit tortor venenatis neque, et tristique justo mi ac erat. Ut nisl quam, vehicula ut ipsum nec, maximus scelerisque turpis. Fusce ac purus placerat, ultricies purus sit amet, blandit libero. Proin tempor id augue non fringilla. Cras ipsum quam, consectetur id vestibulum in, ullamcorper id tortor. Quisque eget nisl ultricies, laoreet lacus vel, vulputate eros. Proin ultricies velit arcu, nec egestas diam convallis quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque at porttitor enim. Nam auctor nibh iaculis consectetur convallis. Aenean imperdiet iaculis metus.
+					</p>
+					<p>
+					Sed sagittis felis tempor vulputate hendrerit. Sed ultricies augue quam, sit amet eleifend quam bibendum non. Nulla quis semper enim. Ut ultrices pulvinar eros, sed dictum lacus congue a. Maecenas ullamcorper ac nulla eget pharetra. Integer id auctor elit. Morbi rutrum sapien in eros interdum convallis. Fusce cursus posuere consectetur. Fusce vel egestas lectus, posuere imperdiet dolor. Morbi efficitur libero sed mattis pretium. In lobortis ultrices augue at viverra. Mauris convallis vehicula ante, id vulputate dolor convallis a.
+					</p>
+					<p>
+					Sed bibendum ipsum sit amet maximus maximus. Vestibulum dapibus id nulla lacinia pharetra. Etiam varius orci justo, a tristique leo finibus eget. Nullam suscipit lobortis urna, nec tempor diam hendrerit in. Vivamus tincidunt, tellus ac rutrum tincidunt, erat tortor vehicula elit, vel aliquet lectus mi eget dolor. Fusce lacinia odio dui. Curabitur aliquet arcu nec tempor dictum. Nam et ornare metus. Aliquam quis rutrum tortor. Maecenas scelerisque lectus in ex dictum dignissim. Suspendisse at purus elit.
+					</p>
+					<p>
+					Quisque euismod tortor nec felis gravida, sit amet molestie nisi finibus. Morbi facilisis, dolor vitae blandit convallis, est lacus congue metus, non dignissim odio nunc facilisis lectus. Aenean luctus mauris in dolor convallis, non aliquet odio viverra. Aenean ut arcu malesuada, molestie dolor in, ullamcorper ante. Sed ut eros quis enim vehicula tincidunt ac sed ante. Aenean ut diam at risus sollicitudin lacinia ut sed velit. Vestibulum nisl odio, porta sed ante a, lacinia gravida est. Fusce et dignissim lacus. Cras sit amet tincidunt urna.
+					</p>
+					<p>
+					Pellentesque auctor et leo eget convallis. Proin nec odio ex. Nullam eros nisl, sagittis ac laoreet sed, pulvinar ac tortor. Maecenas at dolor metus. Quisque id egestas lorem. Morbi a feugiat justo. Proin eget orci quis felis posuere vehicula. Proin ultricies, leo et sollicitudin vulputate, neque libero iaculis diam, non gravida magna nisi non lectus. Etiam ultricies elit et mauris interdum sodales. Morbi dignissim tortor risus, id imperdiet tortor feugiat at. Etiam aliquet fringilla tortor vel ullamcorper. Nullam fringilla nec odio non imperdiet. Ut tincidunt nulla ac turpis porta imperdiet.
+					</p>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet massa sit amet sapien pharetra vulputate. Nullam augue purus, laoreet eget feugiat eget, vulputate sed metus. Quisque ultrices tincidunt velit vitae iaculis. Curabitur at enim tellus. Aenean est est, pulvinar tristique faucibus eget, venenatis eget tortor. Nam et augue porttitor, lacinia odio sed, iaculis sem. Nulla fringilla blandit enim et eleifend. Suspendisse condimentum aliquam commodo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque fringilla neque dictum nisl eleifend placerat. Integer et massa vitae nibh vehicula euismod vitae posuere eros. Integer feugiat pretium tortor, eu bibendum lorem ullamcorper id.
+					</p>
+					<p>
+					In varius auctor lorem, id aliquam turpis pulvinar sit amet. Morbi augue purus, pretium vitae purus nec, cursus aliquet massa. Sed metus tellus, scelerisque aliquet quam porttitor, laoreet volutpat orci. Morbi nulla dui, tempor et varius eu, congue et nibh. Integer vel pulvinar purus, et venenatis lorem. Integer molestie nisi vitae neque mattis, quis luctus leo euismod. Integer congue erat non massa feugiat ultricies. Sed sollicitudin id lorem non euismod.
+					</p>
+					<p>
+					Maecenas et pulvinar leo, vel gravida velit. Proin et massa nec risus dictum varius. Nam id arcu euismod leo fermentum ornare. Pellentesque rhoncus justo et venenatis finibus. Integer ac dictum diam, quis sagittis augue. Ut ornare turpis eu auctor feugiat. Integer ornare dolor bibendum dui eleifend, eget egestas ligula vehicula. Praesent et nulla suscipit, auctor mauris vel, lacinia erat. Ut sed metus consequat, tincidunt velit nec, sagittis quam. Maecenas convallis suscipit elit eget sollicitudin. Donec pellentesque viverra congue. Aenean hendrerit viverra nisl nec porttitor. Vestibulum elit nibh, malesuada eget velit a, bibendum elementum turpis. In blandit lorem at ipsum euismod scelerisque. Mauris ac enim sed diam porttitor hendrerit ullamcorper ut urna. Phasellus eget varius lacus.
+					</p>
+				</article>
+			</section>
+			
+		</section>
+		<footer>
+			All writing, characters, webdesign and artwork are (c) H. Carlian 1997-2016 
+			<br>Fan works are (c) their respective authors, creators and artists.
+		</footer>
+	</div>
+</div>
+
+
+
+
+</body>
